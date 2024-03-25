078d638ee9733b78cd2e8aad4d494f5ee033a02a
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-03-26 02:04:57 +0530

    Delete init.py

--- init.py
+++
@@ -1,7 +0,0 @@
-"""
-The flask application package. This is where we initialize Flask
-"""
-from flask import Flask
-app = Flask(__name__)
-wsgi_app = app.wsgi_app #Registering with IIS
-import FlaskWeb.views
